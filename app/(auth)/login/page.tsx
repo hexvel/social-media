750fbba3ffc05e15c1e0b2cef593ef08f9d9cc81
Hexvel <dimaabukarimov1234@gmail.com>
2024-07-26 23:22:51 +0300

    add auth from google account

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,22 +2,29 @@ import loginImage from "@/assets/login-image.jpg";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import LoginForm from "./_components/LoginForm";
+import GoogleSignInButton from "./_components/google-signin-button";
+import LoginForm from "./_components/login-form";
 
 export const metadata: Metadata = {
   title: "Login",
 };
 
-const LoginPage = () => {
+export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
-          <h1 className="text-center text-3xl font-bold">Login to HexBook</h1>
+          <h1 className="text-center text-3xl font-bold">Login to bugbook</h1>
           <div className="space-y-5">
+            <GoogleSignInButton />
+            <div className="flex items-center gap-3">
+              <div className="h-px flex-1 bg-muted" />
+              <span>OR</span>
+              <div className="h-px flex-1 bg-muted" />
+            </div>
             <LoginForm />
             <Link href="/signup" className="block text-center hover:underline">
-              Don&apos;t have an account? Sign Up
+              Don&apos;t have an account? Sign up
             </Link>
           </div>
         </div>
@@ -29,6 +36,4 @@ const LoginPage = () => {
       </div>
     </main>
   );
-};
-
-export default LoginPage;
+}
--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Google } from "arctic";
 import { Lucia, Session, User } from "lucia";
 import { cookies } from "next/headers";
 import { cache } from "react";
@@ -13,7 +14,7 @@ export const lucia = new Lucia(adapter, {
       secure: process.env.NODE_ENV === "production",
     },
   },
-  getUserAttributes: (databaseUserAttributes) => {
+  getUserAttributes(databaseUserAttributes) {
     return {
       id: databaseUserAttributes.id,
       username: databaseUserAttributes.username,
@@ -24,6 +25,13 @@ export const lucia = new Lucia(adapter, {
   },
 });
 
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
 interface DatabaseUserAttributes {
   id: string;
   username: string;
@@ -32,31 +40,29 @@ interface DatabaseUserAttributes {
   googleId: string | null;
 }
 
-declare module "lucia" {
-  interface Register {
-    Lucia: typeof lucia;
-    DatabaseUserAttributes: DatabaseUserAttributes;
-  }
-}
+export const google = new Google(
+  process.env.GOOGLE_CLIENT_ID!,
+  process.env.GOOGLE_CLIENT_SECRET!,
+  `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/google`,
+);
 
 export const validateRequest = cache(
   async (): Promise<
-    | {
-        user: User;
-        session: Session;
-      }
-    | { user: null; session: null }
+    { user: User; session: Session } | { user: null; session: null }
   > => {
     const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
 
     if (!sessionId) {
-      return { user: null, session: null };
+      return {
+        user: null,
+        session: null,
+      };
     }
 
     const result = await lucia.validateSession(sessionId);
 
     try {
-      if (result.session && result.user) {
+      if (result.session && result.session.fresh) {
         const sessionCookie = lucia.createSessionCookie(result.session.id);
         cookies().set(
           sessionCookie.name,
@@ -64,7 +70,6 @@ export const validateRequest = cache(
           sessionCookie.attributes,
         );
       }
-
       if (!result.session) {
         const sessionCookie = lucia.createBlankSessionCookie();
         cookies().set(
@@ -73,7 +78,7 @@ export const validateRequest = cache(
           sessionCookie.attributes,
         );
       }
-    } catch (error) {}
+    } catch {}
 
     return result;
   },
