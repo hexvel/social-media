9dc0de4ab6b7b9c20d8571d7f96d1b751497f869
Hexvel <dimaabukarimov1234@gmail.com>
2024-07-26 05:53:35 +0300

    add bookmarks

---
+++ app/api/posts/[postId]/bookmark/route.ts
@@ -0,0 +1,88 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { BookmarkInfo } from "@/lib/types";
+
+export default async function GET(
+  req: Request,
+  { params: { postId } }: { params: { postId: string } },
+) {
+  try {
+    const { user: loggedUser } = await validateRequest();
+
+    if (!loggedUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const bookmark = await prisma.bookmark.findUnique({
+      where: {
+        userId_postId: {
+          userId: loggedUser.id,
+          postId,
+        },
+      },
+    });
+
+    const data: BookmarkInfo = {
+      isBookmarkedByUser: !!bookmark,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params: { postId } }: { params: { postId: string } },
+) {
+  try {
+    const { user: loggedUser } = await validateRequest();
+
+    if (!loggedUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.bookmark.upsert({
+      where: {
+        userId_postId: {
+          userId: loggedUser.id,
+          postId,
+        },
+      },
+      create: {
+        userId: loggedUser.id,
+        postId,
+      },
+      update: {},
+    });
+
+    return new Response();
+  } catch (error) {
+    return Response.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params: { postId } }: { params: { postId: string } },
+) {
+  try {
+    const { user: loggedUser } = await validateRequest();
+
+    if (!loggedUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.bookmark.deleteMany({
+      where: {
+        userId: loggedUser.id,
+        postId,
+      },
+    });
+
+    return new Response();
+  } catch (error) {
+    return Response.json({ error: "Something went wrong" }, { status: 500 });
+  }
+}
--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -11,6 +11,7 @@ import PostsMoreButton from "../posts-more-button";
 import { useSession } from "../providers/session-provider";
 import UserAvatar from "../user-avatar";
 import UserTooltip from "../user-tooltip";
+import BookmarkButton from "./bookmark-button";
 import LikeButton from "./like-button";
 
 interface PostsProps {
@@ -61,13 +62,23 @@ export default function Post({ post }: PostsProps) {
         <MediaPreviews attachments={post.attachments} />
       )}
       <hr className="text-muted-foreground" />
-      <LikeButton
-        postId={post.id}
-        initialState={{
-          likes: post._count.likes,
-          isLikedByUser: !!post.likes.some((like) => like.userId === user.id),
-        }}
-      />
+      <div className="flex justify-between gap-5">
+        <LikeButton
+          postId={post.id}
+          initialState={{
+            likes: post._count.likes,
+            isLikedByUser: !!post.likes.some((like) => like.userId === user.id),
+          }}
+        />
+        <BookmarkButton
+          postId={post.id}
+          initialState={{
+            isBookmarkedByUser: post.bookmarks.some(
+              (bookmark) => bookmark.userId === user.id,
+            ),
+          }}
+        />
+      </div>
     </article>
   );
 }
