f16611b2cc79b52ad13bc9d6e60b158661cc4f66
Hexvel <dimaabukarimov1234@gmail.com>
2024-07-26 23:09:21 +0300

    fix

--- app/api/posts/[postId]/likes/route.ts
+++ app/api/posts/[postId]/likes/route.ts
@@ -2,7 +2,7 @@ import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { LikeInfo } from "@/lib/types";
 
-export default async function GET(
+export async function GET(
   req: Request,
   { params }: { params: { postId: string } },
 ) {
